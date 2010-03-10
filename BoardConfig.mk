52d4e8e031d851d010996c792865251cbee6444f
Koushik K. Dutta <koushd@gmail.com>
2010-03-10 13:43:12 -0800

    Use proper MAX_SIZE values

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,10 +35,10 @@ BOARD_GPS_LIBRARIES := libmoto_gps
 
 USE_PV_WINDOWS_MEDIA := false
 
-BOARD_BOOTIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00280000)
-BOARD_RECOVERYIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00500000)
-BOARD_SYSTEMIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x04380000)
-BOARD_USERDATAIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x04ac0000)
+BOARD_BOOTIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00380000)
+BOARD_RECOVERYIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x08c60000)
+BOARD_SYSTEMIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x08c60000)
+BOARD_USERDATAIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x105c0000)
 # The size of a block that can be marked bad.
 BOARD_FLASH_BLOCK_SIZE := 131072
 
