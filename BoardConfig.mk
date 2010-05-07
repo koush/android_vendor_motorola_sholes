02b3396c24ade0691d76142a3a98a567572672ce
Steve Kondik <shade@chemlab.org>
2010-05-07 12:22:46 -0400

    Move ALLOW_LGPL and ENABLE_ANIMATED_GIF to board config.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,3 +47,7 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 # BOARD_KERNEL_BASE := 0x20000000
 USE_CAMERA_STUB := false
 BOARD_EGL_CFG := vendor/motorola/sholes-open/egl.cfg
+
+ENABLE_ANIMATED_GIF := true
+ALLOW_LGPL := true
+
